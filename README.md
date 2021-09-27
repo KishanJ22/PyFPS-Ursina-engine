eab722afd561f8720ef8e72952d6d67dd6c12b14
KishanJ22 <kishanjethwa224@gmail.com>
2021-09-27 14:15:22 +0100

    Version 0.2 - I have added walls to the map, a sky background and increased the player speed, time spent in the air and the height of the jump. Success criteria met: The map must function properly by being 3D and having walls
    Success criteria partially completed:
    The window will default to full screen but the user should be able to change the resolution - currently it is windowed
    Mappable keys on the keyboard will be used to move the player, change the weapon, view the map - currently the player can only move and jump

---
+++ README.md
@@ -0,0 +1,1 @@
+# PyFPS-Ursina-engine
--- main.py
+++ main.py
@@ -13,16 +13,37 @@ class Map():
     ground = Entity(model='plane',
                     texture='brick',
                     collider='mesh',
-                    scale=(100, 1, 100))
+                    scale=(500, 100, 500))
+    wall_left = Entity(model='cube',
+                       texture='wall',
+                       collider='mesh',
+                       scale=(500, 10, 1),  # scale=(x,y,z)
+                       position=(0, 5, 249.5))  # position=(right,up,forward)
+    wall_right = duplicate(wall_left, position=(0, 5, -249.5))
+    wall_front = duplicate(wall_left,
+                           position=(249.5, 5, 0),
+                           rotation_y=90)
+    wall_back = duplicate(wall_front,
+                          position=(-249.5, 5, 0))
+    Sky()  # sets the background to the sky for a better appearance
 
-    player = FirstPersonController()
-    # Has basic controls for moving the player in a first person game
-    # Controls:
-    # W = Move forward
-    # A = Move left
-    # D = Move right
-    # S = Move Backwards
-    # Space Bar = Jump
+
+class Player(Entity):
+    player = FirstPersonController(
+        position=(5, 0, 200),
+        speed=10,  # the speed of the player movement
+        air_time=3,  # the amount of seconds spent in the air when the player jumps
+        jump_height=50  # the height the player reaches when it jumps
+    )
+
+
+# Has basic controls for moving the player in a first person game
+# Controls:
+# W = Move forward
+# A = Move left
+# D = Move right
+# S = Move Backwards
+# Space Bar = Jump
 
 
 app.run()
