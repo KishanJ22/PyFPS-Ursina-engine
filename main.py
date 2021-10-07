55a1e789b52a254b52d4c0ab75decd5043983766
KishanJ22 <kishanjethwa224@gmail.com>
2021-10-07 14:16:15 +0100

    Version 0.3 - I have started to develop the Enemy module by adding a sprite that is present on the map.
    Success criteria to be completed in this version:
    The enemy will go to the player when spawned in
    The map must allow AI enemies to spawn in random areas.
    
    I have faced trouble with moving the sprite and making it follow the player. In version 0.3.1, the enemy will be able to move.

--- main.py
+++ main.py
@@ -4,36 +4,39 @@ from ursina import *
 
 # imports the Ursina Engine
 
-app = Ursina()
+app = Ursina(vsync=True)
 
 
 # initializes the Ursina app
 
-class Map():
+class Map:
     ground = Entity(model='plane',
                     texture='brick',
-                    collider='mesh',
-                    scale=(500, 100, 500))
+                    color=color.light_gray,
+                    collider='mesh',  # the wall will be made up of polygons that the player cannot go through
+                    scale=(500, 100, 500))  # scale=(x,y,z)
     wall_left = Entity(model='cube',
-                       texture='wall',
+                       texture='brick',
+                       color=color.dark_gray,
                        collider='mesh',
                        scale=(500, 10, 1),  # scale=(x,y,z)
                        position=(0, 5, 249.5))  # position=(right,up,forward)
-    wall_right = duplicate(wall_left, position=(0, 5, -249.5))
+    wall_right = duplicate(wall_left, position=(0, 5, -249.5))  # copies wall_left and changes the position of the wall
     wall_front = duplicate(wall_left,
                            position=(249.5, 5, 0),
-                           rotation_y=90)
+                           rotation_y=90)  # copies wall_left, changes the position of the wall and rotates it
     wall_back = duplicate(wall_front,
-                          position=(-249.5, 5, 0))
+                          position=(-249.5, 5, 0))  # copies wall_left and changes the position of the wall
     Sky()  # sets the background to the sky for a better appearance
 
 
 class Player(Entity):
     player = FirstPersonController(
-        position=(5, 0, 200),
-        speed=10,  # the speed of the player movement
+        position=(5, 0, 195),
+        speed=30,  # the speed of the player movement
         air_time=3,  # the amount of seconds spent in the air when the player jumps
-        jump_height=50  # the height the player reaches when it jumps
+        jump_height=10,  # the height the player reaches when it jumps
+        height=5
     )
 
 
@@ -44,6 +47,22 @@ class Player(Entity):
 # D = Move right
 # S = Move Backwards
 # Space Bar = Jump
+
+
+class Enemy(Entity):
+    enemy = Entity(model="cube",
+                   texture="enemy_tommy.png",  # uses the image "enemy_tommy.png" on the cube
+                   collider="mesh",  # adds collision detection to the enemy sprite
+                   scale=(1, 2.5, 0),  # scaled so that the sprite is just about the right size for the player camera
+                   position=(5, 0, 190))
+
+
+def update():
+    Enemy.enemy.look_at(Player.player.world_position)
+    # uses the position of the player on the map to look at the player
+    Enemy.enemy.position += Enemy.enemy.forward
+    # enemy position goes forward slowly when the player moves
+    Enemy.enemy.y = 3
 
 
 app.run()
