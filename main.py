25149e223d78cdd96eaaddca97e2f55665207952
KishanJ22 <kishanjethwa224@gmail.com>
2021-10-19 14:20:49 +0100

    Version 0.3.1 - I have developed the Enemy module by adding a sprite that is present on the map. It now looks at the player and moves towards the player.
    Success criteria completed in this version:
    The enemy will go to the player when spawned in
    
    I have faced trouble with moving the sprite and making it follow the player. In version 0.3.1, the enemy will be able to move.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import time
+
 from ursina.prefabs.first_person_controller import FirstPersonController
 # this will allow the user to move using WASD and use space bar to jump
 from ursina import *
 
 # imports the Ursina Engine
 
-app = Ursina(vsync=True)
+app = Ursina()
+window.title = "PyFPS"
 
 
 # initializes the Ursina app
@@ -27,13 +30,17 @@ class Map:
                            rotation_y=90)  # copies wall_left, changes the position of the wall and rotates it
     wall_back = duplicate(wall_front,
                           position=(-249.5, 5, 0))  # copies wall_left and changes the position of the wall
+    object1 = Entity(model='plane',
+                     color=color.white,
+                     collider='mesh',
+                     scale=(10, 100, 1))
     Sky()  # sets the background to the sky for a better appearance
 
 
 class Player(Entity):
     player = FirstPersonController(
         position=(5, 0, 195),
-        speed=30,  # the speed of the player movement
+        speed=20,  # the speed of the player movement
         air_time=3,  # the amount of seconds spent in the air when the player jumps
         jump_height=10,  # the height the player reaches when it jumps
         height=5
@@ -54,15 +61,25 @@ class Enemy(Entity):
                    texture="enemy_tommy.png",  # uses the image "enemy_tommy.png" on the cube
                    collider="mesh",  # adds collision detection to the enemy sprite
                    scale=(1, 2.5, 0),  # scaled so that the sprite is just about the right size for the player camera
-                   position=(5, 0, 190))
-
+                   position=(5, 1.2, 190))
 
 def update():
-    Enemy.enemy.look_at(Player.player.world_position)
-    # uses the position of the player on the map to look at the player
-    Enemy.enemy.position += Enemy.enemy.forward
-    # enemy position goes forward slowly when the player moves
-    Enemy.enemy.y = 3
+    #Enemy.enemy.look_at(Vec3(Player.player.x, Enemy.enemy.y, Player.player.z)) # uses the x coordinate and z coordinate
+    Enemy.enemy.look_at(Player.player)
+    # of the player and the y coordinate of the enemy
+    Enemy.enemy.position += Enemy.enemy.forward*50
+    # this uses the enemy position and makes it go forward by 50 frames
+    Enemy.enemy.y = 1.2
+    # This keeps the y-axis value of the enemy position at 1.2 so that it doesn't go through the floor
+
+class Gameplay():
+    Player = Player()
+    Enemy = Enemy()
+
+# uses the position of the player on the map to look at the player
+# Enemy.enemy.y = 3
+# Enemy.enemy.position += Enemy.enemy.forward * 0.05
+# enemy position goes forward slowly when the player moves
 
 
 app.run()
