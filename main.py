44c8c1e4386752ccad924b13613e062831cb413e
KishanJ22 <kishanjethwa224@gmail.com>
2021-10-19 14:20:49 +0100

    Version 0.3.2 - I have developed the Enemy module further by implementing a raycaster to prevent the enemies from going into each other and adding a for loop to spawn multiple enemies at random locations on the map.
    Success criteria completed in this version:
    The map must allow AI enemies to spawn in random areas
    
    I have faced trouble with moving the sprite and making it follow the player. In version 0.3.1, the enemy will be able to move.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import time
-
 from ursina.prefabs.first_person_controller import FirstPersonController
 # this will allow the user to move using WASD and use space bar to jump
 from ursina import *
+from ursina.raycaster import raycast
 
 # imports the Ursina Engine
 
@@ -57,24 +57,58 @@ class Player(Entity):
 
 
 class Enemy(Entity):
-    enemy = Entity(model="cube",
-                   texture="enemy_tommy.png",  # uses the image "enemy_tommy.png" on the cube
-                   collider="mesh",  # adds collision detection to the enemy sprite
-                   scale=(1, 2.5, 0),  # scaled so that the sprite is just about the right size for the player camera
-                   position=(5, 1.2, 190))
+    def __init__(self, position):
+        super().__init__(
+            model="cube",
+            texture="enemy_tommy.png",  # uses the image "enemy_tommy.png" on the cube
+            collider="mesh",  # adds collision detection to the enemy sprite
+            scale=(1, 2.5, 0),  # scale=(x,y,z)
+            position=position)
 
-def update():
-    #Enemy.enemy.look_at(Vec3(Player.player.x, Enemy.enemy.y, Player.player.z)) # uses the x coordinate and z coordinate
-    Enemy.enemy.look_at(Player.player)
-    # of the player and the y coordinate of the enemy
-    Enemy.enemy.position += Enemy.enemy.forward*50
-    # this uses the enemy position and makes it go forward by 50 frames
-    Enemy.enemy.y = 1.2
-    # This keeps the y-axis value of the enemy position at 1.2 so that it doesn't go through the floor
+    def update(self):
+        # Enemy.enemy.look_at(Vec3(Player.player.x, Enemy.enemy.y, Player.player.z)) # uses the x coordinate and z coordinate
+        self.look_at(Player.player)
+        # of the player and the y coordinate of the enemy
+        self.position += self.forward * 80
+        # this uses the enemy position and makes it go forward by 50 frames
+        self.y = 1.2
+        # This keeps the y-axis value of the enemy position at 1.2 so that it doesn't go through the floor
+        origin = self.world_position
+        # sets the start point for the ray (line 80)
+        self.direction = [self.forward, self.back, self.left, self.right]
+        # the different directions are stored in an array
+        random_direction = random.choice(self.direction)
+        # the self.direction array is shuffled
+        randint = random.randint(100, 500)
+        # numbers in the range of 100 to 500 are randomised
+        ray = raycast(origin, random_direction, distance=.5)
+        '''
+        This casts a ray from the position of the enemy, in a random direction picked from the variable
+        'random_direction' and sets the distance of the ray to 0.5
+        This has been done because every enemy has a mesh collider, which means that when a ray from an enemy hits
+        another enemy, it will know that it shouldn't go into that enemy.
+        '''
+        print(ray)
+        # outputs when a ray has been hit
+        if not ray.hit:
+            self.position += random_direction
+        # if the ray doesn't hit any object with a collider, it will move in a random direction
 
-class Gameplay():
+
+class Gameplay:
     Player = Player()
-    Enemy = Enemy()
+    enemies = []
+    # the array for enemy instances
+    for x in range(3):
+        # spawns 3 enemies in by running this loop 3 times
+        random_spawn_position = random.randint(100, 200)
+# this creates a random number between 100 and 200 so that it can be used as the 'Z' parameter in the position attribute
+# this is then used in enemy_instance so that each enemy spawns at a random location on the map when the game starts
+        enemy_instance = Enemy(position=Vec3(x, 0, random_spawn_position))
+        # creates an instance of Enemy and changes 'random_spawn_position' is used to generate a random spawn point
+        enemies.append(enemy_instance)
+        # puts each instance in the enemies array
+
 
 # uses the position of the player on the map to look at the player
 # Enemy.enemy.y = 3
